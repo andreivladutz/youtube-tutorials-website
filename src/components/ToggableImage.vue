069e09a98eee04a5311a1eb82292103e8ec519c9
Andrei Vladutz <andrei.vladlt@gmail.com>
2020-08-22 21:59:25 +0300

    Changed theme shipped svgs with proprietary ones

--- src/components/ToggableImage.vue
+++ src/components/ToggableImage.vue
@@ -1,56 +1,47 @@
 <template>
   <div :class="groupClass">
-    <!-- <img
-      :class="[imgClass, 'asset-light']"
-      :src="assetsFilepath(lightFile)"
-      :alt="imgAlt"
-    /> -->
-    <img
-      :class="[imgClass, 'asset-dark']"
-      :src="assetsFilepath(darkFile)"
-      :alt="imgAlt"
-    />
-    <object
+    <img :class="[imgClass, 'asset-light']" :src="assetsFilepath(lightFile)" :alt="imgAlt" />
+    <img :class="[imgClass, 'asset-dark']" :src="assetsFilepath(darkFile)" :alt="imgAlt" />
+    <!-- <object
       type="image/svg+xml"
       :data="assetsFilepath(lightFile)"
       :class="[imgClass, 'asset-light']"
     >
       {{ imgAlt }}
-    </object>
+    </object>-->
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  props: {
-    // The container div's class
-    groupClass: {
-      type: String,
-      required: true,
+  import Vue from "vue";
+  export default Vue.extend({
+    props: {
+      // The container div's class
+      groupClass: {
+        type: String,
+        required: true
+      },
+      // The images' class
+      imgClass: {
+        type: String,
+        required: true
+      },
+      // The image alt
+      imgAlt: {
+        type: String,
+        required: true
+      },
+      lightFile: {
+        type: String,
+        required: true
+      },
+      darkFile: {
+        type: String,
+        required: true
+      }
     },
-    // The images' class
-    imgClass: {
-      type: String,
-      required: true,
-    },
-    // The image alt
-    imgAlt: {
-      type: String,
-      required: true,
-    },
-    lightFile: {
-      type: String,
-      required: true,
-    },
-    darkFile: {
-      type: String,
-      required: true,
-    },
-  },
-  methods: {
-    assetsFilepath: (filename: string) =>
-      require(`@/assets/images/${filename}`),
-  },
-});
+    methods: {
+      assetsFilepath: (filename: string) => require(`@/assets/images/${filename}`)
+    }
+  });
 </script>
